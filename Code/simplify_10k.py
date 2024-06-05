2ac900140503d33d7003f4969f81ef15d7188f66
John <jgfriedman99@gmail.com>
2024-06-05 11:42:27 -0700

    changed from decomposition to attributes, added visualization

--- Code/simplify_10k.py
+++ Code/simplify_10k.py
@@ -29,6 +29,15 @@ with open(path) as f:
 soup = BeautifulSoup(html, 'html.parser')
 body = soup.find('body')
 
+# reminder to look for element-type, as we will be using that attribute
+#TODO
+
+# remove existing background colors from all elements
+# Note: coded this quickly using copilot, did not check
+for element in body.find_all():
+    if element.has_attr("style"):
+        element['style'] = re.sub(r'background-color:[^;]{1,}','',element['style'])
+
 # remove hidden elements
 # may need to tweak
 for element in body.select('[style*="display: none"]'):
@@ -36,10 +45,12 @@ for element in body.select('[style*="display: none"]'):
 for element in body.select('[style*="display:none"]'):
     element.decompose()
 
+
 simplified_soup = BeautifulSoup("", 'html.parser')
 
 
 # mutability is something to keep in mind
+# changing approach to mark in attributes
 def recursive_simplification(element):
     
     # may remove
@@ -47,48 +58,43 @@ def recursive_simplification(element):
         # if find specific tags, stop else
         if element.name == 'p':
             if element.text.strip() == "":
-                element.decompose()
+                element['element-type'] = 'empty'
+                return
             else:
-                new_tag = simplified_soup.new_tag('p')
-                new_tag.string = element.text
-                # stores if element was parsed
-                new_tag['element-type'] = 'parsed'
+                element['element-type'] = 'p:'
 
                 if detect_bolded_text(element):
-                    add_style(new_tag, 'font-weight: bold;')
+                    element['element-type'] += 'bold;'
 
                 if detect_italicized_text(element):
-                    add_style(new_tag,'font-style: italic;')
-
-                simplified_soup.append(new_tag)
-                element.decompose()
+                    element['element-type'] += 'italic;'
+                return
         elif element.name == 'span':
             if element.text.strip() == "":
-                element.decompose()
+                element['element-type'] = 'empty'
+                return
             else:
-                new_tag = simplified_soup.new_tag('p')
-                new_tag.string = element.text
-                # stores if element was parsed
-                new_tag['element-type'] = 'parsed'
-                # test if self is bolded or italicized
+                element['element-type'] = 'span:'
+
                 if detect_bolded_text(element):
-                    add_style(new_tag, 'font-weight: bold;')
+                    element['element-type'] += 'bold;'
 
                 if detect_italicized_text(element):
-                    add_style(new_tag,'font-style: italic;')
-
-                simplified_soup.append(new_tag)
-                element.decompose()
+                    element['element-type'] += 'italic;'
+                return
         # remove breaks
         elif element.name == 'br':
-            element.decompose()
+            element['element-type'] = 'line-break'
+            return
         # remove table of contents
         elif element.name == 'a':
             if element.text.strip().lower() == 'table of contents':
-                element.decompose()
+                element['element-type'] = 'toc-link'
+                return
         # ignore tables for now
         elif element.name in ['table']:
-            add_element(element,simplified_soup)
+                element['element-type'] = 'table'
+                return
         else:
             # i think we can safely ignore navigable strings
             children = [child for child in element.findChildren(recursive=False) if isinstance(child, Tag)]
@@ -96,12 +102,25 @@ def recursive_simplification(element):
                 for child in children:
                     recursive_simplification(child)
             else:
-                add_element(element,simplified_soup)
+                return
     
-
+# color
+def color_parsing(soup):
+    # change to a list of colors
+    color_list = ['Cornsilk','BlanchedAlmond','Bisque','NavajoWhite','Wheat','BurlyWood','Tan','RosyBrown','SandyBrown','Goldenrod','DarkGoldenrod','Peru','Chocolate','SaddleBrown','Sienna','Brown','Maroon']
+    # hard code colors
+    # need to tweak
+    color_dict = {}
+    for element in soup.find_all(attrs={'element-type': True}):
+        element_type = element['element-type']
+        if element_type in color_dict.keys():
+            add_style(element, f"background-color:{color_dict[element_type]};")
+        else:
+            color = color_list.pop(0)
+            color_dict[element_type] = color
+            add_style(element, f"background-color:{color};")
     
 
 recursive_simplification(body)
-open_soup(simplified_soup)
-#soup = BeautifulSoup(html, 'html.parser')
-#open_soup(soup)
+color_parsing(body)
+open_soup(soup)
