fa661a562a408a5aa717f1893df29c2d0a2da85f
John <jgfriedman99@gmail.com>
2024-06-22 21:10:12 -0700

    fixed up examples

--- sec_parsers/setup.py
+++ sec_parsers/setup.py
@@ -3,14 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="sec_parsers",
     author="John Friedman",
-    version="0.401",
+    version="0.402",
     description = "A package to parse SEC filings",
     packages=find_packages(),
     install_requires=[
         'beautifulsoup4',
-        'pandas',
-        're',
-        'tempfile',
         'pandas'   
     ],
 )
