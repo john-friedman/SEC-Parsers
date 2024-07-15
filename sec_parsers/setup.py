7c43dec50db286f0aa0186dd5f88ee1291778b79
John <jgfriedman99@gmail.com>
2024-07-15 12:20:35 -0700

    fixed dependencies missing requests

--- sec_parsers/setup.py
+++ sec_parsers/setup.py
@@ -6,12 +6,13 @@ long_description = Path("../readme.md").read_text()
 setup(
     name="sec_parsers",
     author="John Friedman",
-    version="0.528",
+    version="0.529",
     description = "A package to parse SEC filings",
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'lxml', 
+        'requests',
     ],
 )
